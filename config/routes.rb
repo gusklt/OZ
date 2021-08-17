e9607248d4d756d04e560faa2e4d0ffe93ffba42
Rogerio DaSilva <rogerioa.farinha@gmail.com>
2021-08-17 14:36:24 +0200

    bookings-new added

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show]
   root to: 'pages#home'
   resources :performances, only: [:index] do
-    resources :booking, only: [:new, :create, :show]
+    resources :bookings, only: [:new, :create]
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
