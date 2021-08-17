c78c05b0184dff7dacd29810f15ca4c74c4a429b
Rogerio DaSilva <rogerioa.farinha@gmail.com>
2021-08-17 17:48:48 +0200

    booking status update and edit

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :performances, only: [:index] do
-    resources :bookings, only: [:new, :create]
+    resources :bookings, only: [:new, :create, :edit, :update]
   end
 
   resources :performances
