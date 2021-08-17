642c75c6262e85ed28ca4b675f38a318da260c2f
thais de rodez <thaisderodez@gmail.com>
2021-08-17 12:31:01 +0200

    show route, action and view

--- app/controllers/performances_controller.rb
+++ app/controllers/performances_controller.rb
@@ -1,6 +1,11 @@
 class PerformancesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     @performances = Performance.all
+  end
+
+  def show
+    @performance = Performance.find(params[:id])
   end
 end
