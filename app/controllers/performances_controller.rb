8fbcfcea9301612c9429a3793e29009c52eb259e
thais de rodez <thaisderodez@gmail.com>
2021-08-17 14:23:59 +0200

    new and create action

--- app/controllers/performances_controller.rb
+++ app/controllers/performances_controller.rb
@@ -8,4 +8,24 @@ class PerformancesController < ApplicationController
   def show
     @performance = Performance.find(params[:id])
   end
+
+  def new
+    @performance = Performance.new
+  end
+
+  def create
+    @performance = Performance.new(performance_params)
+    @performance.user = current_user
+    if @performance.save
+      redirect_to performance_path(@performance)
+    else
+      redirect_to new_performance_path
+    end
+  end
+
+  private
+
+  def performance_params
+    params.require(:performance).permit(:title, :location, :description, :price, :photo)
+  end
 end
