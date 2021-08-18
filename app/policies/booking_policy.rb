69fa8f5c6da3d7298aa04086af5d35fff3520387
Rogerio DaSilva <rogerioa.farinha@gmail.com>
2021-08-18 10:55:39 +0200

    pundit bookings

---
+++ app/policies/booking_policy.rb
@@ -0,0 +1,24 @@
+class BookingPolicy < ApplicationPolicy
+
+  def create?
+    true
+  end
+
+  def new?
+    create?
+  end
+
+  def update?
+    record.user == user
+  end
+
+  def edit?
+    update?
+  end
+
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+end
