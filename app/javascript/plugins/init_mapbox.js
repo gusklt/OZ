3a5a40ebc808bcf0d3da4840279016161f9595c0
Augustin Clot <augustin.clot@gmail.com>
2021-08-18 15:50:51 +0200

    map colored, cards changed to product-card, background color changed

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -32,7 +32,7 @@ const initMapbox = () => {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10'
+      style: 'mapbox://styles/clotaugu/ckshjkht06acu17uq0h6gezqv'
     });
 
     const markers = JSON.parse(mapElement.dataset.markers);
