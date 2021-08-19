946e95e62612f2e9ed8aa0729a251038c7799930
Augustin Clot <augustin.clot@gmail.com>
2021-08-19 17:26:50 +0200

    work is in progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,13 @@
 Rails.application.routes.draw do
-  get 'reviews/new'
-  get 'reviews/create'
-  get 'reviews/index'
-  get 'reviews/show'
   devise_for :users
   resources :users, only: [:show]
   root to: 'pages#home'
 
-  resources :performances, only: [:index] do
-    resources :bookings, only: [:new, :create, :edit, :update, :index]
+  resources :performances do
+    resources :bookings, only: [:new, :create, :edit, :update, :index] do
+      resources :reviews, only: [:new, :create]
+    end
   end
-
-  resources :performances
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
