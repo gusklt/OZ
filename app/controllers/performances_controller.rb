8730cedf36e56b5dbad711eeeb9fc8fa50252827
Augustin Clot <augustin.clot@gmail.com>
2021-08-20 11:41:34 +0200

    having some changes

--- app/controllers/performances_controller.rb
+++ app/controllers/performances_controller.rb
@@ -71,4 +71,6 @@ class PerformancesController < ApplicationController
   def performance_params
     params.require(:performance).permit(:title, :location, :description, :price, :photo)
   end
+
+  # rubocop:enable Metrics/MethodLength
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,14 @@
 class ReviewsController < ApplicationController
-  def new
-  end
 
   def create
+    @performance = Performance.find(params[:performance_id])
+    @review = Review.new(review_params)
+    @review.performance = @performance
+    if @review.save
+      redirect_to performance_path(@performance)
+    else
+      render 'performances/show'
+    end
   end
 
   def index
@@ -10,4 +16,11 @@ class ReviewsController < ApplicationController
 
   def show
   end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+
 end
